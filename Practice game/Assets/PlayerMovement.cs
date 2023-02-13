9ee03101039d0d3c0895a2f1103388a95b1681b7
IshanRajput024 <102878135+IshanRajput024@users.noreply.github.com>
2023-02-13 19:43:56 +0530

    y

--- Practice game/Assets/PlayerMovement.cs
+++ Practice game/Assets/PlayerMovement.cs
@@ -4,27 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private Animator anim;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
-    void Update()
-    { 
-       if(Input.GetKeyDown("space"))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
-        }
+    private void Update()
+    {
+        float dirx = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirx * 7f, rb.velocity.y);
 
-        if (Input.GetKeyDown("d"))
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(15, 0, 0);
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
+
         }
-        if (Input.GetKeyDown("a"))
+        if (dirx > 0f)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(-15, 0, 0);
+            anim.SetBool("running", true);
+        }
+        else if (dirx < 0f)
+        { 
+            anim.SetBool("running", true);
+        }
+        else
+        {
+            anim.SetBool("running",false);
+
+
         }
     }
 }
